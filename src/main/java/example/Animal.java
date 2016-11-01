96ffe9a0efa09556735a8d828c4a403056c7d5d7
Michael Gfeller <mgfeller@mgfeller.net>
2016-11-01 22:35:12 +0100

    examples

---
+++ src/main/java/example/Animal.java
@@ -0,0 +1,15 @@
+package example;
+
+public class Animal {
+  public boolean canHop() {
+    return true;
+  }
+
+  public boolean canSwim() {
+    return false;
+  }
+
+  public void makeSound() {
+    System.out.println("quack");
+  }
+}
--- src/main/java/example/LambdaExpressions.java
+++ src/main/java/example/LambdaExpressions.java
@@ -1,9 +1,28 @@
 package example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
 public class LambdaExpressions {
 
-  public static void main(String[] args) {
-    System.out.println("Hello");
+  @FunctionalInterface
+  interface CheckTrait {
+    boolean test(Animal a);
   }
 
+  static CheckTrait swimmer1 = a -> a.canSwim();
+  static CheckTrait swimmer2 = (Animal a) -> a.canSwim();
+  static CheckTrait swimmer3 = (Animal a) -> { return a.canSwim(); };
+
+  static Supplier<Animal> creator1 = () -> new Animal();
+  static Supplier<Animal> creator2 = Animal::new;
+
+  public static void main(String[] args) {
+    List<Animal> animals = new ArrayList<>();
+    animals.add(creator1.get());
+    animals.stream().forEach(a -> swimmer3.test(a));
+    animals.stream().filter(a -> a.canSwim() && a.canHop()).count();
+    animals.stream().forEach(Animal::makeSound);
+  }
 }
